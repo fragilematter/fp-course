d9435f8d5ec4f7ec65e1e9fb8692cd1ac2cfe08d
octavia <db@0db.ro>
2025-04-27 16:09:08 +0300

    functional head, tail, take implementations

--- 04_product_types/app/Main.hs
+++ 04_product_types/app/Main.hs
@@ -1,4 +1,41 @@
 module Main where
 
+{-
+data Easy = Bool | Bool Bool
+
+twotwo :: Bool -> Bool
+twotwo b = True
+
+-- data Medium = Bool | twotwo
+-}
+
+leHead :: String -> Char
+leHead s = case s of
+            [] -> error "don't ask for head on an empty string"
+            (x:_) -> x
+
+leTail :: String -> String
+leTail s = case s of
+            [] -> error "don't chase tail of an empty string"
+            (_:x) -> x
+
+leNull :: String -> Bool
+leNull s = case s of
+            [] -> True
+            _ -> False
+
+
+leTake :: Int -> String -> String
+leTake leCount leStr = case (leCount, leStr) of
+            (0, _) -> []
+            (_, []) -> []
+            (n, s) -> leHead s : leTake (n - 1) (leTail s)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    let whereIsMyHead = leHead "hello" 
+    putStrLn [whereIsMyHead]
+    let wagYourTail = leTail "hello"
+    putStrLn wagYourTail
+    let takeOnMe = leTake 4 "take on meeeee"
+    putStrLn takeOnMe
